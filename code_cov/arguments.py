3eb8023c4c23b200c3d9782183c17ddd3f32c23d
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-28 21:05:52 +0800

    Add Subcommand

--- code_cov/arguments.py
+++ code_cov/arguments.py
@@ -1,6 +1,54 @@
+import abc
 import argparse
+from collections.abc import Callable
+
+from typing_extensions import Self
 
 
-def create_parser() -> argparse.ArgumentParser:
+class Subcommand(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self: Self, args: argparse.Namespace) -> None:
+        raise NotImplementedError()
+
+    @classmethod
+    @abc.abstractmethod
+    def setup_parser(cls: type[Self], parser: argparse.ArgumentParser) -> None:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def run(self: Self) -> None:
+        raise NotImplementedError()
+
+
+_subcommand_name_types: dict[str, type[Subcommand]] = dict()
+
+
+def subcommand(
+    name: str,
+    subcommand_name_types: dict[str, type[Subcommand]] = _subcommand_name_types,
+) -> Callable[[type[Subcommand]], type[Subcommand]]:
+    def subcommand_decorator(cls: type[Subcommand]) -> type[Subcommand]:
+        subcommand_name_types[name] = cls
+        return cls
+    return subcommand_decorator
+
+
+def create_parser(
+    subcommand_name_types: dict[str, type[Subcommand]] = _subcommand_name_types,
+) -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='subcommand')
+    for subcommand_name, subcommand_type in subcommand_name_types.items():
+        subparser = subparsers.add_parser(subcommand_name)
+        subcommand_type = subcommand_type.setup_parser(subparser)
     return parser
+
+
+def create_subcommand(
+    name: str,
+    args: argparse.Namespace,
+    subcommand_name_types: dict[str, type[Subcommand]] = _subcommand_name_types,
+) -> Subcommand:
+    subcommand_type = subcommand_name_types[name]
+    subcommand = subcommand_type(args)
+    return subcommand
---
+++ tests/unit/test_arguments.py
@@ -0,0 +1,59 @@
+import argparse
+import unittest
+
+from typing_extensions import Self
+
+from code_cov import arguments
+
+
+class ArgumentsTest(unittest.TestCase):
+    def test_subcommand__typical_case__adds_to_subcommand_name_types(self):
+        subcommand_name_types = dict()
+        self.assertEqual(dict(), subcommand_name_types)
+        @arguments.subcommand(
+            'command', subcommand_name_types=subcommand_name_types)
+        class SubcommandStub(arguments.Subcommand):
+            pass
+        self.assertEqual(dict(command=SubcommandStub), subcommand_name_types)
+
+    def test_create_parser__typical_case__creates_successfully(self):
+        subcommand_name_types = dict()
+        self.assertEqual(dict(), subcommand_name_types)
+        @arguments.subcommand(
+            'command', subcommand_name_types=subcommand_name_types)
+        class SubcommandStub(arguments.Subcommand):
+            @classmethod
+            def setup_parser(
+                cls: type[Self],
+                parser: argparse.ArgumentParser,
+            ) -> None:
+                pass
+        parser = (arguments
+            .create_parser(subcommand_name_types=subcommand_name_types))
+        args = parser.parse_args(['command'])
+        self.assertEqual('command', args.subcommand)
+
+    def test_create_subcommand__typical_case__creates_successfully(self):
+        subcommand_name_types = dict()
+        self.assertEqual(dict(), subcommand_name_types)
+        @arguments.subcommand(
+            'command', subcommand_name_types=subcommand_name_types)
+        class SubcommandStub(arguments.Subcommand):
+            def __init__(self: Self, args: argparse.Namespace) -> None:
+                pass
+
+            @classmethod
+            def setup_parser(
+                cls: type[Self],
+                parser: argparse.ArgumentParser,
+            ) -> None:
+                pass
+
+            def run(self: Self) -> None:
+                pass
+        parser = (arguments
+            .create_parser(subcommand_name_types=subcommand_name_types))
+        args = parser.parse_args(['command'])
+        subcommand = arguments.create_subcommand(
+            args.subcommand, args, subcommand_name_types=subcommand_name_types)
+        self.assertIsInstance(subcommand, SubcommandStub)
