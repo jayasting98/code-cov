78a8ce5393efb565e17be75476ec2bdd7af9927c
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-30 00:40:23 +0800

    Implement create_trainer

--- code_cov/trainer_factories.py
+++ code_cov/trainer_factories.py
@@ -1,3 +1,4 @@
+from typing import Any
 from typing import TypedDict
 
 import datasets
@@ -5,7 +6,7 @@ import transformers
 
 
 class TrainerInfo(TypedDict, total=False):
-    pass
+    data_collator_config: dict[str, Any]
 
 
 def create_trainer(
@@ -16,4 +17,9 @@ def create_trainer(
     training_arguments: transformers.TrainingArguments,
     model: transformers.PreTrainedModel,
 ) -> transformers.Trainer:
-    pass
+    data_collator_config = trainer_info.get('data_collator_config', dict())
+    data_collator = (transformers
+        .DataCollatorForSeq2Seq(tokenizer, model=model, **data_collator_config))
+    trainer = transformers.Trainer(model=model, args=training_arguments,
+        data_collator=data_collator, train_dataset=dataset, tokenizer=tokenizer)
+    return trainer
