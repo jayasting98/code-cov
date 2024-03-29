ea792de295a817467b3d7200fc4a6817e91b626d
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-29 17:08:11 +0800

    Add Processor

--- code_cov/data_processors.py
+++ code_cov/data_processors.py
@@ -1,19 +1,63 @@
+import abc
 from typing import Any
 from typing import TypedDict
 
 import datasets
 import transformers
 from typing_extensions import Required
+from typing_extensions import Self
+
+from code_cov import utilities
+
+
+class ProcessorInfo(TypedDict, total=False):
+    _alias: Required[str]
+    config: dict[str, Any]
+
+
+class Processor(abc.ABC):
+    @abc.abstractmethod
+    def process(
+        self: Self,
+        dataset: datasets.Dataset,
+        tokenizer: (transformers.PreTrainedTokenizer
+            | transformers.PreTrainedTokenizerFast),
+    ) -> datasets.Dataset:
+        raise NotImplementedError()
+
+
+_processor_alias_types: dict[str, type[Processor]] = dict()
+
+
+processor = utilities.create_object_alias_decorator(_processor_alias_types)
 
 
 class DatasetConfig(TypedDict, total=False):
     loader_config: Required[dict[str, Any]]
+    processor_infos: list[ProcessorInfo]
+
+
+def _create_processor(
+    processor_info: ProcessorInfo,
+    processor_alias_types: dict[str, type[Processor]] = _processor_alias_types,
+) -> Processor:
+    alias = processor_info['_alias']
+    config = processor_info.get('config', dict())
+    type_ = processor_alias_types[alias]
+    processor = type_(**config)
+    return processor
 
 
 def create_dataset(
     dataset_config: DatasetConfig,
     tokenizer: (transformers.PreTrainedTokenizer
         | transformers.PreTrainedTokenizerFast),
+    processor_alias_types: dict[str, type[Processor]] = _processor_alias_types,
 ) -> datasets.Dataset:
     dataset = datasets.load_dataset(**dataset_config['loader_config'])
+    processor_infos = dataset_config.get('processor_infos', list())
+    for processor_info in processor_infos:
+        processor = _create_processor(
+            processor_info, processor_alias_types=processor_alias_types)
+        dataset = processor.process(dataset, tokenizer)
     return dataset
--- tests/unit/test_data_processors.py
+++ tests/unit/test_data_processors.py
@@ -2,12 +2,15 @@ import unittest
 from unittest import mock
 
 import datasets
+from typing_extensions import Self
+
+import transformers
 
 from code_cov import data_processors
 
 
 class DataProcessorsTest(unittest.TestCase):
-    def test_create_dataset__typical_case__creates_correctly(self):
+    def test_create_dataset__with_no_processors__creates_correctly(self):
         loader_config = dict(path='path/to/dataset')
         dataset_config = (
             data_processors.DatasetConfig(loader_config=loader_config))
@@ -22,5 +25,43 @@ class DataProcessorsTest(unittest.TestCase):
         actual_iter = iter(actual_dataset)
         self.assertEqual(dict(message='Hello'), next(actual_iter))
         self.assertEqual(dict(message='World!'), next(actual_iter))
+        with self.assertRaises(StopIteration):
+            next(actual_iter)
+
+    def test_create_dataset__with_processors__creates_correctly(self):
+        processor_alias_types = dict()
+        @data_processors.processor('stub', alias_objects=processor_alias_types)
+        class ProcessorStub(data_processors.Processor):
+            def __init__(self, multiplier) -> None:
+                self._multiplier = multiplier
+
+            def process(
+                self: Self,
+                dataset: datasets.Dataset,
+                tokenizer: (transformers.PreTrainedTokenizer
+                    | transformers.PreTrainedTokenizerFast),
+            ) -> datasets.Dataset:
+                def multiply(x):
+                    x['num'] *= self._multiplier
+                    return x
+                processed_ds = dataset.map(multiply)
+                return processed_ds
+        loader_config = dict(path='path/to/dataset')
+        processor_infos = [data_processors
+            .ProcessorInfo(_alias='stub', config=dict(multiplier=3))]
+        dataset_config = data_processors.DatasetConfig(
+            loader_config=loader_config, processor_infos=processor_infos)
+        tokenizer = mock.MagicMock()
+        expected_dataset = (
+            datasets.Dataset.from_dict(dict(num=[13, 5, 8])))
+        with mock.patch('datasets.load_dataset') as load_dataset_mock:
+            load_dataset_mock.return_value = expected_dataset
+            actual_dataset = data_processors.create_dataset(dataset_config,
+                tokenizer, processor_alias_types=processor_alias_types)
+        load_dataset_mock.assert_called_once_with(path='path/to/dataset')
+        actual_iter = iter(actual_dataset)
+        self.assertEqual(dict(num=39), next(actual_iter))
+        self.assertEqual(dict(num=15), next(actual_iter))
+        self.assertEqual(dict(num=24), next(actual_iter))
         with self.assertRaises(StopIteration):
             next(actual_iter)
