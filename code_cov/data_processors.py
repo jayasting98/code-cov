7cd58d3fd2ebbb6418afa4842fc67356ae660f2b
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-29 21:02:03 +0800

    Add PromptAndTargetGenerator

--- code_cov/data_processors.py
+++ code_cov/data_processors.py
@@ -84,6 +84,109 @@ class NonConsecutiveSplitter(Processor):
         return split_samples
 
 
+@processor('generate_prompt_and_target')
+class PromptAndTargetGenerator(Processor):
+    _OPENING_TAG_COMMENT_TEMPLATE: str = '/* <{name}> */'
+    _CLOSING_TAG_COMMENT_TEMPLATE: str = '/* </{name}> */'
+
+    def __init__(
+        self: Self,
+        prompt_template: str,
+        focal_method_tag_name: str,
+        focal_lines_tag_name: str,
+        test_input_method_tag_name: str,
+        test_target_method_tag_name: str,
+    ) -> None:
+        self._prompt_template = prompt_template
+        self._focal_method_tag_name = focal_method_tag_name
+        self._focal_lines_tag_name = focal_lines_tag_name
+        self._test_input_method_tag_name = test_input_method_tag_name
+        self._test_target_method_tag_name = test_target_method_tag_name
+
+    def process(
+        self: Self,
+        dataset: datasets.Dataset,
+        tokenizer: (transformers.PreTrainedTokenizer
+            | transformers.PreTrainedTokenizerFast),
+    ) -> datasets.Dataset:
+        processed_ds = dataset.map(
+            self._generate_prompt_and_target,
+            remove_columns=dataset.column_names)
+        return processed_ds
+
+    def _generate_prompt_and_target(
+        self: Self,
+        sample: dict[str, Any],
+    ) -> dict[str, Any]:
+        prompt_focal_class = sample['focal_class']['identifier']
+        focal_method = sample['focal_method']
+        focal_method_line_start = focal_method['line_start']
+        focal_method_body: str = focal_method['body']
+        focal_line_indices = sample['focal_line_indices']
+        indices = [focal_line_index - focal_method_line_start
+            for focal_line_index in focal_line_indices]
+        focal_method_lines = focal_method_body.split('\n')
+        focal_method_closing_tag_comment = (
+            self._create_closing_tag_comment(self._focal_method_tag_name))
+        focal_method_lines.insert(
+            len(focal_method_lines), focal_method_closing_tag_comment)
+        focal_lines_closing_tag_comment = (
+            self._create_closing_tag_comment(self._focal_lines_tag_name))
+        focal_method_lines.insert(
+            indices[-1] + 1, focal_lines_closing_tag_comment)
+        focal_lines_opening_tag_comment = (
+            self._create_opening_tag_comment(self._focal_lines_tag_name))
+        focal_method_lines.insert(
+            indices[0], focal_lines_opening_tag_comment)
+        focal_method_opening_tag_comment = (
+            self._create_opening_tag_comment(self._focal_method_tag_name))
+        focal_method_lines.insert(0, focal_method_opening_tag_comment)
+        test_input_method = sample['test_input_method']
+        test_input_method_body: str = test_input_method['body']
+        test_input_method_lines = test_input_method_body.split('\n')
+        test_input_method_closing_tag_comment = (
+            self._create_closing_tag_comment(self._test_input_method_tag_name))
+        test_input_method_lines.insert(
+            len(test_input_method_lines), test_input_method_closing_tag_comment)
+        test_input_method_opening_tag_comment = (
+            self._create_opening_tag_comment(self._test_input_method_tag_name))
+        test_input_method_lines.insert(
+            0, test_input_method_opening_tag_comment)
+        test_target_method = sample['test_target_method']
+        test_target_method_body: str = test_target_method['body']
+        test_target_method_lines = test_target_method_body.split('\n')
+        test_target_method_closing_tag_comment = (
+            self._create_closing_tag_comment(self._test_target_method_tag_name))
+        test_target_method_lines.insert(len(test_target_method_lines),
+            test_target_method_closing_tag_comment)
+        test_target_method_opening_tag_comment = (
+            self._create_opening_tag_comment(self._test_target_method_tag_name))
+        test_target_method_lines.insert(
+            0, test_target_method_opening_tag_comment)
+        prompt_focal_method = '\n'.join(focal_method_lines)
+        prompt_test_input_method = '\n'.join(test_input_method_lines)
+        prompt_test_target_method = '\n'.join(test_target_method_lines)
+        prompt = self._prompt_template.format(focal_class=prompt_focal_class,
+            focal_method=prompt_focal_method,
+            test_input_method=prompt_test_input_method)
+        prompt_sample = dict(prompt=prompt, target=prompt_test_target_method)
+        return prompt_sample
+
+    def _create_opening_tag_comment(self: Self, name: str) -> str:
+        opening_tag_comment = (
+            self._create_tag_comment(self._OPENING_TAG_COMMENT_TEMPLATE, name))
+        return opening_tag_comment
+
+    def _create_closing_tag_comment(self: Self, name: str) -> str:
+        closing_tag_comment = (
+            self._create_tag_comment(self._CLOSING_TAG_COMMENT_TEMPLATE, name))
+        return closing_tag_comment
+
+    def _create_tag_comment(self: Self, template: str, name: str) -> str:
+        tag_comment = template.format(name=name)
+        return tag_comment
+
+
 class DatasetConfig(TypedDict, total=False):
     loader_config: Required[dict[str, Any]]
     processor_infos: list[ProcessorInfo]
--- tests/unit/test_data_processors.py
+++ tests/unit/test_data_processors.py
@@ -156,3 +156,163 @@ class NonConsecutiveSplitterTest(unittest.TestCase):
         self.assertEqual(expected_sample_6, next(actual_iter))
         with self.assertRaises(StopIteration):
             next(actual_iter)
+
+
+class PromptAndTargetGeneratorTest(unittest.TestCase):
+    def test_create_dataset__typical_case__creates_correctly(self):
+        loader_config = dict(path='path/to/dataset')
+        processor_config = dict(
+            prompt_template=(''
+                + '// Focal class: {focal_class}\n'
+                + '// Focal method:\n'
+                + '{focal_method}\n'
+                + '// Test method:\n'
+                + '{test_input_method}'),
+            focal_method_tag_name='method',
+            focal_lines_tag_name='code',
+            test_input_method_tag_name='input',
+            test_target_method_tag_name='edited',
+        )
+        processor_info = data_processors.ProcessorInfo(
+            _alias='generate_prompt_and_target', config=processor_config)
+        processor_infos = [processor_info]
+        dataset_config = data_processors.DatasetConfig(
+            loader_config=loader_config, processor_infos=processor_infos)
+        tokenizer = mock.MagicMock()
+        expected_dataset = datasets.Dataset.from_dict(dict(
+            focal_class=[
+                dict(identifier='SomeClass'),
+                dict(identifier='SomeOtherClass'),
+            ],
+            focal_method=[
+                dict(
+                    line_start=13,
+                    body=('static void isEven(int x) {\n'
+                        + '        if (x % 2 == 0) {\n'
+                        + '            boolean value = true;\n'
+                        + '            return value;\n'
+                        + '        } else {\n'
+                        + '            return false;\n'
+                        + '        }\n'
+                        + '    }'),
+                ),
+                dict(
+                    line_start=34,
+                    body=('static void isOdd(int x) {\n'
+                        + '        if (x % 2 == 0) {\n'
+                        + '            boolean value = false;\n'
+                        + '            return value;\n'
+                        + '        } else {\n'
+                        + '            return true;\n'
+                        + '        }\n'
+                        + '    }'),
+                ),
+            ],
+            focal_line_indices=[
+                [15, 16],
+                [39],
+            ],
+            test_input_method=[
+                dict(
+                    body=('@Test\n'
+                        + '    void testIsEven_odd_false() {\n'
+                        + '        assertFalse(SomeClass.isEven(5));\n'
+                        + '    }'),
+                ),
+                dict(
+                    body=('@Test\n'
+                        + '    void testIsOdd_even_false() {\n'
+                        + '        assertFalse(SomeOtherClass.isOdd(8));\n'
+                        + '    }'),
+                ),
+            ],
+            test_target_method=[
+                dict(
+                    body=('@Test\n'
+                        + '    void testIsEven_even_true() {\n'
+                        + '        assertTrue(SomeClass.isEven(8));\n'
+                        + '    }'),
+                ),
+                dict(
+                    body=('@Test\n'
+                        + '    void testIsOdd_odd_true() {\n'
+                        + '        assertTrue(SomeOtherClass.isOdd(5));\n'
+                        + '    }'),
+                ),
+            ],
+            other_key=[
+                'is',
+                'ignored',
+            ]
+        ))
+        with mock.patch('datasets.load_dataset') as load_dataset_mock:
+            load_dataset_mock.return_value = expected_dataset
+            actual_dataset = (
+                data_processors.create_dataset(dataset_config, tokenizer))
+        actual_iter = iter(actual_dataset)
+        expected_sample_0 = dict(
+            prompt=(''
+                + '// Focal class: SomeClass\n'
+                + '// Focal method:\n'
+                + '/* <method> */\n'
+                + 'static void isEven(int x) {\n'
+                + '        if (x % 2 == 0) {\n'
+                + '/* <code> */\n'
+                + '            boolean value = true;\n'
+                + '            return value;\n'
+                + '/* </code> */\n'
+                + '        } else {\n'
+                + '            return false;\n'
+                + '        }\n'
+                + '    }\n'
+                + '/* </method> */\n'
+                + '// Test method:\n'
+                + '/* <input> */\n'
+                + '@Test\n'
+                + '    void testIsEven_odd_false() {\n'
+                + '        assertFalse(SomeClass.isEven(5));\n'
+                + '    }\n'
+                + '/* </input> */'),
+            target=(''
+                + '/* <edited> */\n'
+                + '@Test\n'
+                + '    void testIsEven_even_true() {\n'
+                + '        assertTrue(SomeClass.isEven(8));\n'
+                + '    }\n'
+                + '/* </edited> */'),
+        )
+        self.assertEqual(expected_sample_0, next(actual_iter))
+        expected_sample_1 = dict(
+            prompt=(''
+                + '// Focal class: SomeOtherClass\n'
+                + '// Focal method:\n'
+                + '/* <method> */\n'
+                + 'static void isOdd(int x) {\n'
+                + '        if (x % 2 == 0) {\n'
+                + '            boolean value = false;\n'
+                + '            return value;\n'
+                + '        } else {\n'
+                + '/* <code> */\n'
+                + '            return true;\n'
+                + '/* </code> */\n'
+                + '        }\n'
+                + '    }\n'
+                + '/* </method> */\n'
+                + '// Test method:\n'
+                + '/* <input> */\n'
+                + '@Test\n'
+                + '    void testIsOdd_even_false() {\n'
+                + '        assertFalse(SomeOtherClass.isOdd(8));\n'
+                + '    }\n'
+                + '/* </input> */'),
+            target=(''
+                + '/* <edited> */\n'
+                + '@Test\n'
+                + '    void testIsOdd_odd_true() {\n'
+                + '        assertTrue(SomeOtherClass.isOdd(5));\n'
+                + '    }\n'
+                + '/* </edited> */'),
+        )
+        self.assertEqual(expected_sample_1, next(actual_iter))
+        with self.assertRaises(StopIteration):
+            next(actual_iter)
