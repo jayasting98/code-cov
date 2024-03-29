daf50ace45ecb72ae9084e32349dba847ff5e707
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-30 00:52:04 +0800

    Add Google Cloud Storage token support

--- code_cov/subcommands.py
+++ code_cov/subcommands.py
@@ -15,10 +15,12 @@ from code_cov import trainer_factories
 class TrainSubcommand(arguments.Subcommand):
     def __init__(self: Self, args: argparse.Namespace) -> None:
         self._config_file_pathname: str = args.config_path
+        self._token: dict[str, str] = args.token
 
     @classmethod
     def setup_parser(cls: type[Self], parser: argparse.ArgumentParser) -> None:
         parser.add_argument('--config_path', required=True)
+        parser.add_argument('--token', default=None, type=json.loads)
 
     def run(self: Self) -> None:
         with open(self._config_file_pathname) as config_file:
@@ -26,6 +28,11 @@ class TrainSubcommand(arguments.Subcommand):
         tokenizer_config: dict[str, Any] = config['tokenizer_config']
         dataset_config = (
             data_processors.DatasetConfig(**config['dataset_config']))
+        try:
+            dataset_config['loader_config']['storage_options']['token'] = (
+                self._token)
+        except KeyError:
+            pass
         training_arguments_config: dict[str, Any] = (
             config['training_arguments_config'])
         model_info = model_factories.ModelInfo(**config['model_info'])
