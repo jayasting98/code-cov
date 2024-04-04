f9b6b26a84c6e7cac66c0e062305c1414679dc71
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-04 20:09:27 +0800

    Add EvaluateSubcommand

--- code_cov/subcommands.py
+++ code_cov/subcommands.py
@@ -2,16 +2,25 @@ import argparse
 import json
 import logging
 import math
+import os
+import pathlib
+import re
+import tempfile
+import traceback
 from typing import Any
 
+import git
 import torch
 import transformers
 from typing_extensions import Self
 
 from code_cov import arguments
+from code_cov import coverages
 from code_cov import data_collator_factories
 from code_cov import data_processors
 from code_cov import model_factories
+from code_cov import projects
+from code_cov import utilities
 
 
 @arguments.subcommand('train')
@@ -191,3 +200,206 @@ class GenerateSubcommand(arguments.Subcommand):
             return candidate_samples
         candidate_ds = dataset.map(generate, batched=True, **map_config)
         candidate_ds.save_to_disk(**saver_config)
+
+
+@arguments.subcommand('evaluate')
+class EvaluateSubcommand(arguments.Subcommand):
+    def __init__(self: Self, args: argparse.Namespace) -> None:
+        self._config_file_pathname: str = args.config_path
+        self._log_level: str = args.log_level.upper()
+        self._token: dict[str, str] = args.token
+
+    @classmethod
+    def setup_parser(cls: type[Self], parser: argparse.ArgumentParser) -> None:
+        parser.add_argument('--config_path', required=True)
+        parser.add_argument('--log_level', default='warning')
+        parser.add_argument('--token', default=None, type=json.loads)
+
+    def run(self: Self) -> None:
+        logging.basicConfig(
+            format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+            level=self._log_level,
+            datefmt='%Y-%m-%d %H:%M:%S',
+        )
+        logging.info('parsing config')
+        with open(self._config_file_pathname) as config_file:
+            config = json.load(config_file)
+        dataset_config = (
+            data_processors.DatasetConfig(**config['dataset_config']))
+        try:
+            dataset_config['loader_config']['storage_options']['token'] = (
+                self._token)
+        except KeyError:
+            # If the key 'storage_options' does not exist, then the token does
+            # not apply, so we can just pass.
+            pass
+        code_cov_config = config['code_cov_config']
+        output_file_pathname = config['output_path']
+        skip = config['skip']
+        limit = config['limit']
+        regexps = config['regexps']
+        test_candidate_method_re = re.compile(regexps['candidate'])
+        test_method_name_re = re.compile(regexps['test_method_name'])
+        test_class_body_re = re.compile(regexps['test_class_body'])
+        logging.info('creating dataset')
+        dataset = data_processors.create_dataset(dataset_config, None)
+        logging.info('creating code cov')
+        code_cov = coverages.CodeCovCli(**code_cov_config)
+        ds_iter = iter(dataset)
+        for _ in zip(range(skip), ds_iter):
+            pass
+        repository_url_datas = dict()
+        for i, sample in zip(range(skip, skip + limit), ds_iter):
+            repository = sample['repository']
+            url = repository['repository_url']
+            if url not in repository_url_datas:
+                logging.info(f'sample {i}: repository {url}')
+                temp_dir = tempfile.TemporaryDirectory()
+                project_path_datas = dict()
+                repository_data = dict(dir=temp_dir,
+                    projects=project_path_datas)
+                repository_url_datas[url] = repository_data
+                try:
+                    logging.info(f'sample {i}: cloning')
+                    git.Repo.clone_from(url, temp_dir.name)
+                except Exception as exception:
+                    logging.warning(f'{exception}: {url}')
+                    logging.debug(f'{traceback.format_exc()}')
+                    continue
+            repository_data = repository_url_datas[url]
+            project_path_datas = repository_data['projects']
+            temp_dir = repository_data['dir']
+            project_relative_pathname = sample['project_path']
+            project_pathname = (
+                os.path.join(temp_dir.name, project_relative_pathname))
+            if project_relative_pathname not in project_path_datas:
+                logging.info(f'sample {i}: project {project_relative_pathname}')
+                project = (
+                    projects.create_project(temp_dir.name, project_pathname))
+                logging.info(f'sample {i}: finding classpath pathnames')
+                classpath_pathnames = project.find_classpath_pathnames()
+                logging.info(f'sample {i}: finding focal classpath')
+                focal_classpath = project.find_focal_classpath()
+                project_data = dict(project=project,
+                    classpath_pathnames=classpath_pathnames,
+                    focal_classpath=focal_classpath,
+                    sample_statistics=dict())
+                project_path_datas[project_relative_pathname] = project_data
+            project_data = project_path_datas[project_relative_pathname]
+            project: projects.Project = project_data['project']
+            classpath_pathnames = project_data['classpath_pathnames']
+            focal_classpath = project_data['focal_classpath']
+            focal_class = sample['focal_class']
+            focal_package = focal_class['package']
+            focal_class_identifier = focal_class['identifier']
+            focal_class_name = f'{focal_package}.{focal_class_identifier}'
+            test_class = sample['test_class']
+            test_package = test_class['package']
+            test_class_identifier = test_class['identifier']
+            test_class_name = f'{test_package}.{test_class_identifier}'
+            test_file_relative_pathname = sample['test_file']
+            test_file_pathname = (
+                os.path.join(project_pathname, test_file_relative_pathname))
+            candidate = sample['candidate']
+            focal_line_indices = sample['focal_line_indices']
+            sample_id = (sample['focal_method']['identifier'],
+                sample['test_input_method']['identifier'],
+                sample['test_target_method']['identifier'],
+                focal_line_indices[0], focal_line_indices[-1])
+            if sample_id not in project_data['sample_statistics']:
+                logging.info(f'sample {i}: {sample_id}')
+                project_data['sample_statistics'][sample_id] = (
+                    dict(total=0, correct=0))
+            project_data['sample_statistics'][sample_id]['total'] += 1
+            test_candidate_method_match = (
+                test_candidate_method_re.search(candidate))
+            if test_candidate_method_match is None:
+                # incorrect sample
+                logging.info(f'sample {i}: test candidate method not found')
+                logging.debug(f'{candidate}')
+                continue
+            test_candidate_method = test_candidate_method_match[1]
+            logging.info(f'sample {i}: test candidate method found')
+            logging.debug(f'{test_candidate_method}')
+            test_candidate_method_name_match = (
+                test_method_name_re.search(test_candidate_method))
+            if test_candidate_method_name_match is None:
+                # incorrect sample
+                logging.info(
+                    f'sample {i}: test candidate method name not found')
+                continue
+            test_candidate_method_name = test_candidate_method_name_match[1]
+            logging.info(f'sample {i}: test candidate method name found '
+                + f'`{test_candidate_method_name}`')
+            with utilities.TemporaryChangeFile(test_file_pathname):
+                with open(test_file_pathname) as test_file:
+                    test_file_content = test_file.read()
+                test_class_body_match = (
+                    test_class_body_re.search(test_file_content))
+                if test_class_body_match is None:
+                    logging.warning(f'sample {i}: test class not found: '
+                        + f'{test_file_pathname}')
+                    continue
+                test_class_body = test_class_body_match[1]
+                test_candidate_file_content = (test_file_content
+                    .replace(test_class_body, test_candidate_method))
+                with open(test_file_pathname, mode='w') as test_file:
+                    test_file.write(test_candidate_file_content)
+                logging.info(f'sample {i}: compiling')
+                try:
+                    project.compile()
+                except Exception:
+                    # incorrect sample
+                    logging.info(f'sample {i}: failed to compile')
+                    continue
+                request_data = coverages.CreateCoverageRequestData(
+                    classpathPathnames=classpath_pathnames,
+                    focalClasspath=focal_classpath,
+                    focalClassName=focal_class_name,
+                    testClassName=test_class_name,
+                    testMethodName=test_candidate_method_name,
+                )
+                try:
+                    with utilities.WorkingDirectory(project_pathname):
+                        coverage = code_cov.create_coverage(request_data)
+                except Exception as exception:
+                    # incorrect sample
+                    logging.info(f'sample {i}: coverage not found')
+                    continue
+            is_fully_covered = True
+            for focal_line_index in focal_line_indices:
+                focal_line_number = focal_line_index + 1
+                if focal_line_number not in coverage['coveredLineNumbers']:
+                    logging.info(
+                        f'sample {i}: line {focal_line_number} not covered')
+                    is_fully_covered = False
+                    break
+            if not is_fully_covered:
+                # incorrect sample
+                continue
+            logging.info(f'sample {i}: correct')
+            project_data['sample_statistics'][sample_id]['correct'] += 1
+        output_data: dict[str, Any] = dict()
+        for repository_url, repository_data in repository_url_datas.items():
+            temp_dir: tempfile.TemporaryDirectory = repository_data['dir']
+            temp_dir.cleanup()
+            project_path_datas: dict[str, Any] = repository_data['projects']
+            output_repository_data: dict[str, list] = dict()
+            for project_id, project_data in project_path_datas.items():
+                output_project_data = list()
+                sample_statistics: dict[str, dict[str, int]] = (
+                    project_data['sample_statistics'])
+                for sample_id, sample_data in sample_statistics.items():
+                    output_sample_data = dict(**sample_data)
+                    output_sample_data['focal_method'] = sample_id[0]
+                    output_sample_data['test_input_method'] = sample_id[1]
+                    output_sample_data['test_target_method'] = sample_id[2]
+                    output_sample_data['line_index_start'] = sample_id[3]
+                    output_sample_data['line_index_end'] = sample_id[4]
+                    output_project_data.append(output_sample_data)
+                output_repository_data[project_id] = output_project_data
+            output_data[repository_url] = output_repository_data
+        (pathlib.Path(output_file_pathname)
+            .parent.mkdir(parents=True, exist_ok=True))
+        with open(output_file_pathname, mode='w') as output_file:
+            json.dump(output_data, output_file, indent=4)
